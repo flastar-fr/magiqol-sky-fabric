b0c73f40cdce88138e8b04c98883b9e48346b72d
flastar-fr <140191575+flastar-fr@users.noreply.github.com>
2025-12-11 01:44:21 +0100

    Update information

---
+++ src/main/java/fr/flastar/magiqolsky/MagicalQoLSkyClient.java
@@ -0,0 +1,10 @@
+package fr.flastar.magiqolsky;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicalQoLSkyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
