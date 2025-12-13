56917ec28460514f4bcdbdb5a034b446a8cf13d8
Lazerstricks <lazerstricks0IQ@gmail.com>
2025-12-13 23:50:50 +0100

    chore: add log

--- src/main/java/fr/flastar/magiqolsky/shopitems/ShopItemCreator.java
+++ src/main/java/fr/flastar/magiqolsky/shopitems/ShopItemCreator.java
@@ -1,5 +1,6 @@
 package fr.flastar.magiqolsky.shopitems;
 
+import fr.flastar.magiqolsky.MagiQoLSky;
 import fr.flastar.magiqolsky.shopitems.config.ShopConfig;
 
 import java.util.HashMap;
@@ -21,6 +22,10 @@ public class ShopItemCreator {
         for (Map<String, Float> categoryItems : allCategories.values()) {
             shopItems.putAll(categoryItems);
         }
+        shopConfig.getAllShopItems().forEach((cat, items) ->
+                MagiQoLSky.LOGGER.info("{} -> {} items", cat, items.size())
+        );
+
     }
 
     public HashMap<String, Float> getShopItems() {
