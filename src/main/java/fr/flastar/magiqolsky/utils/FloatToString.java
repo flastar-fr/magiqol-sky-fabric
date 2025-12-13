8c7b8416bd7cddd21d1ff5dc941d6b0b8a191e6b
flastar-fr <140191575+flastar-fr@users.noreply.github.com>
2025-12-13 15:37:45 +0100

    Adapt removeDecimals method to game language + adapt decimal separator to game language while displaying a container value

--- src/main/java/fr/flastar/magiqolsky/utils/FloatToString.java
+++ src/main/java/fr/flastar/magiqolsky/utils/FloatToString.java
@@ -1,25 +1,42 @@
 package fr.flastar.magiqolsky.utils;
 
-public class FloatToString {
+import net.minecraft.client.MinecraftClient;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
+public class FloatToString {
     public static String convertDecimalFloatToString(float f, int precision) {
+        Locale clientLocale = getClientLocale();
         String formatString = "%." + precision + "f";
 
-        String formattedString = String.format(formatString, f);
+        String formattedString = String.format(clientLocale, formatString, f);
+        char decimalSeparator = new DecimalFormatSymbols(clientLocale).getDecimalSeparator();
 
-        return removeDecimals(formattedString);
+        return removeDecimals(formattedString, decimalSeparator);
     }
 
-    public static String removeDecimals(String number) {
-        boolean containsDecimal = number.contains(",");
-        boolean endWith0OrDot = number.endsWith("0") || number.endsWith(",");
+    public static String removeDecimals(String number, char decimalSeparator) {
+        String decimalSeparatorSequence = Character.toString(decimalSeparator);
+
+        boolean containsDecimal = number.contains(decimalSeparatorSequence);
+        boolean endWith0OrDot = number.endsWith("0") || number.endsWith(decimalSeparatorSequence);
 
         while (endWith0OrDot && number.length() > 1 && containsDecimal) {
             number = number.substring(0, number.length() - 1);
-            containsDecimal = number.contains(",");
-            endWith0OrDot = number.endsWith("0") || number.endsWith(",");
+            containsDecimal = number.contains(decimalSeparatorSequence);
+            endWith0OrDot = number.endsWith("0") || number.endsWith(decimalSeparatorSequence);
         }
 
         return number;
+    }
+
+    public static Locale getClientLocale() {
+        MinecraftClient client = MinecraftClient.getInstance();
+
+        String localeCode = client.getLanguageManager().getLanguage();
+
+        String languageTag = localeCode.replace('_', '-');
+
+        return Locale.forLanguageTag(languageTag);
     }
 }
