a5b8c64d536956d7ac40d1205d1930a3b35fc628
flastar-fr <140191575+flastar-fr@users.noreply.github.com>
2025-12-13 14:50:52 +0100

    Improve value render by removing trailing zeros and decimals

--- src/main/java/fr/flastar/magiqolsky/utils/FloatToString.java
+++ src/main/java/fr/flastar/magiqolsky/utils/FloatToString.java
@@ -5,6 +5,21 @@ public class FloatToString {
     public static String convertDecimalFloatToString(float f, int precision) {
         String formatString = "%." + precision + "f";
 
-        return String.format(formatString, f);
+        String formattedString = String.format(formatString, f);
+
+        return removeDecimals(formattedString);
+    }
+
+    public static String removeDecimals(String number) {
+        boolean containsDecimal = number.contains(",");
+        boolean endWith0OrDot = number.endsWith("0") || number.endsWith(",");
+
+        while (endWith0OrDot && number.length() > 1 && containsDecimal) {
+            number = number.substring(0, number.length() - 1);
+            containsDecimal = number.contains(",");
+            endWith0OrDot = number.endsWith("0") || number.endsWith(",");
+        }
+
+        return number;
     }
 }
