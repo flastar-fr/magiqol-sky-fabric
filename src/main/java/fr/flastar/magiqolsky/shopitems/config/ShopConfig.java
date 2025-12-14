84c4e85a7320c8cd0102ab0513ee5527a5b58713
flastar-fr <140191575+flastar-fr@users.noreply.github.com>
2025-12-14 12:19:24 +0100

    Remove unused import

--- src/main/java/fr/flastar/magiqolsky/shopitems/config/ShopConfig.java
+++ src/main/java/fr/flastar/magiqolsky/shopitems/config/ShopConfig.java
@@ -11,7 +11,6 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
