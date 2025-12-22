df2a3541a4aac45fa55fd338a2848f0b7caf5fd7
flastar-fr <140191575+flastar-fr@users.noreply.github.com>
2025-12-22 23:40:34 +0100

    Add choice of hour format in chat manager

--- src/main/java/fr/flastar/magiqolsky/chatmanager/ChatManagerData.java
+++ src/main/java/fr/flastar/magiqolsky/chatmanager/ChatManagerData.java
@@ -11,6 +11,9 @@ public class ChatManagerData {
     private boolean isTextReplacementEnabled;
     private boolean isAutoNightVisionEnabled;
     private boolean isMessageHourEnabled;
+
+    private String messageHourFormat;
+
     private final List<TextReplacerEntry> textReplacers;
 
     private final transient Map<String, String> textReplacersCache = new HashMap<>();
@@ -21,6 +24,8 @@ public class ChatManagerData {
         this.isTextReplacementEnabled = true;
         this.isAutoNightVisionEnabled = false;
         this.isMessageHourEnabled = true;
+        this.messageHourFormat = "'['HH:mm:ss']'";
+
         this.textReplacers = textReplacers;
     }
 
@@ -66,6 +71,14 @@ public class ChatManagerData {
 
     public void changeIsMessageHourEnabled(boolean newValue) {
         isMessageHourEnabled = newValue;
+    }
+
+    public String messageHourFormat() {
+        return messageHourFormat;
+    }
+
+    public void changeMessageHourFormat(String newValue) {
+        messageHourFormat = newValue;
     }
 
     public List<TextReplacerEntry> textReplacers() {
